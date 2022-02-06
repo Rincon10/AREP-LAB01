cb333eba1eddfd0089441e6e6bb6895bec639a36
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-02-06 11:27:02 -0500

    conversions working

--- App/src/main/java/edu/escuelaing/arep/App.java
+++ App/src/main/java/edu/escuelaing/arep/App.java
@@ -3,14 +3,30 @@ package edu.escuelaing.arep;
 import static spark.Spark.*;
 
 /**
- * Hello world!
- *
+ * @author Iván Camilo Rincón Saavedra
+ * @version 1.0 2/1/2022
+ * @project App
  */
-public class App 
-{
+public class App {
+    private static String fahrenheitToCelciusPath = "/celsius/:fahrenheit";
+    private static String celciusTofahrenheitPath = "/fahrenheit/:celsius";
+
+    protected static void setGetControllers() {
+        get(celciusTofahrenheitPath, (req, res) -> {
+            float celsius = Integer.parseInt(req.params(":celsius"));
+            return celsiusToFahrenheit(celsius);
+        });
+
+        get(fahrenheitToCelciusPath, (req, res) -> {
+            float fahrenheit = Integer.parseInt(req.params(":fahrenheit"));
+            return fahrenheitToCelsius(fahrenheit);
+        });
+    }
+
     public static void main(String[] args) {
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        //Setting the Get Controllers of our API
+        setGetControllers();
     }
 
     static int getPort() {
@@ -18,5 +34,13 @@ public class App
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567; //returns default port if heroku-port isn't se  (i.e. on localhost)
+    }
+
+    public static float celsiusToFahrenheit(float celsius) {
+        return (celsius * 1.8f) + 32;
+    }
+
+    public static float fahrenheitToCelsius(float fahrenheit) {
+        return (fahrenheit - 32) / 1.8f;
     }
 }
